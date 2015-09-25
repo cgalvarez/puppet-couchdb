1ce799b3405de4bb8b2af0c0126a85c70118c5f3
Carlos García <cgalvarez.engineer@gmail.com>
2015-09-25 15:35:55 +0200

    Initial version
    Fork of camptocamp/puppet-couchdb v1.1.0

---
+++ manifests/params.pp
@@ -0,0 +1,15 @@
+class couchdb::params {
+
+  $bind_address = $::couchdb_bind_address ? {
+    ''      => '127.0.0.1',
+    default => $::couchdb_bind_address,
+  }
+
+  $port = $::couchdb_port ? {
+    ''      => '5984',
+    default => $::couchdb_port,
+  }
+
+  $backupdir = '/var/backups/couchdb'
+
+}
